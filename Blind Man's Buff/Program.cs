c5b4de4df4234456155d0fafd8aea37d456bf8f1
Stefan Sabev <stevenbg91@gmail.com>
2023-06-24 17:40:20 +0200

    Initial commit

---
+++ Blind Man's Buff/Program.cs
@@ -0,0 +1,107 @@
+﻿int[] ints = Console.ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+char[,] matrix = new char[ints[0], ints[1]];
+int myRow = 0;
+int myCol = 0;
+int moves = 0;
+int touchedOpponents = 0;
+for (int rows = 0; rows < matrix.GetLength(0); rows++)
+{
+    char[] line = Console.ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries).Select(char.Parse).ToArray();
+
+    for (int cols = 0; cols < matrix.GetLength(1); cols++)
+    {
+        //fill our matrix
+        matrix[rows, cols] = line[cols];
+        if (line[cols] == 'B')
+        {
+            myRow = rows;
+            myCol = cols;
+        }
+
+    }
+}
+string command = string.Empty;
+
+while ((command = Console.ReadLine()) != "Finish")
+{
+    if (command == "up")
+    {
+        if (isValidPosition(myRow - 1, myCol, matrix))
+        {
+            if (matrix[myRow - 1, myCol] == 'P')
+            {
+                touchedOpponents++;
+
+            }
+
+            matrix[myRow - 1, myCol] = 'B';
+            matrix[myRow, myCol] = '-';
+            myRow--;
+                moves++;
+
+        }
+    }
+    else if (command == "down")
+    {
+        if (isValidPosition(myRow + 1, myCol, matrix))
+        {
+            if (matrix[myRow + 1, myCol] == 'P')
+            {
+                touchedOpponents++;
+
+            }
+
+            matrix[myRow + 1, myCol] = 'B';
+            matrix[myRow, myCol] = '-';
+            myRow++;
+                moves++;
+        }
+    }
+    else if (command == "left")
+    {
+        if (isValidPosition(myRow, myCol - 1, matrix))
+        {
+            if (matrix[myRow, myCol - 1] == 'P')
+            {
+                touchedOpponents++;
+
+            }
+
+            matrix[myRow, myCol - 1] = 'B';
+            matrix[myRow, myCol] = '-';
+            myCol--;
+                moves++;
+        }
+    }
+    else if (command == "right")
+    {
+        if (isValidPosition(myRow, myCol + 1, matrix))
+        {
+            if (isValidPosition(myRow, myCol + 1, matrix))
+            {
+                if (matrix[myRow, myCol + 1] == 'P')
+                {
+                    touchedOpponents++;
+
+                }
+
+                matrix[myRow, myCol + 1] = 'B';
+                matrix[myRow, myCol] = '-';
+                myCol++;
+                    moves++;
+            }
+        }
+    }
+    if (touchedOpponents == 3)
+    {
+        break;
+    }
+}
+Console.WriteLine("Game over!");
+Console.WriteLine($"Touched opponents: {touchedOpponents} Moves made: {moves}");
+
+bool isValidPosition(int row, int col, char[,] matrix)
+{
+    return row >= 0 && row < matrix.GetLength(0) && col >= 0 && col < matrix.GetLength(1) && matrix[row, col] != 'O';
+}
---
+++ ExamPreparationC#Advanced/Program.cs
@@ -0,0 +1,85 @@
+﻿int[] textile = Console.ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+int[] meds = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+Queue<int> textiles = new Queue<int>(textile);
+Stack<int> medicaments = new Stack<int>(meds);
+
+SortedDictionary<string, int> healingItems = new();
+
+while(textiles.Count > 0 && medicaments.Count > 0)
+{
+    
+    int currentTextile = textiles.Dequeue();
+    int currentMedicament = medicaments.Pop();
+    int sum = currentTextile + currentMedicament;
+    if(sum == 30)
+    {
+        if (!healingItems.ContainsKey("Patch"))
+        {
+            healingItems.Add("Patch", 0);
+        }
+        healingItems["Patch"]++;
+    }
+    else if(sum == 40)
+    {
+        if (!healingItems.ContainsKey("Bandage"))
+        {
+            healingItems.Add("Bandage", 0);
+        }
+        healingItems["Bandage"]++;
+    }
+    else if (sum == 100)
+    {
+        if (!healingItems.ContainsKey("MedKit"))
+        {
+            healingItems.Add("MedKit", 0);
+        }
+        healingItems["MedKit"]++;
+    }
+    else if (sum > 100)
+    {
+        if (!healingItems.ContainsKey("MedKit"))
+        {
+            healingItems.Add("MedKit", 0);
+        }
+        healingItems["MedKit"]++;
+        sum -= 100;
+        int currentMedicamentToPush = medicaments.Pop();
+        medicaments.Push(currentMedicamentToPush + sum);
+    }
+    else
+    {
+        currentMedicament += 10;
+        medicaments.Push(currentMedicament);
+    }
+
+}
+if (medicaments.Count == 0 && textiles.Count == 0 )
+{
+    Console.WriteLine("Textiles and medicaments are both empty.");
+}
+else if (medicaments.Count == 0)
+{
+    Console.WriteLine("Medicaments are empty.");
+}
+else
+{
+    Console.WriteLine("Textiles are empty.");
+}
+
+foreach (var item in healingItems.OrderByDescending(x => x.Value).ThenBy(x => x.Key))
+{
+    Console.WriteLine($"{item.Key} - {item.Value}");
+}
+if (medicaments.Count > 0)
+{
+
+    Console.Write("Medicaments left: ");
+    Console.WriteLine(string.Join(", ", medicaments));
+}
+else if (textiles.Count > 0)
+{
+    Console.Write("Textiles left: ");
+
+    Console.WriteLine(string.Join(", ", textiles));
+}
